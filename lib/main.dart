987f0698d8a3db1498fb0ec33726787c82417ce4
fharhanamrin028@gmail.com <fharhanamrin028@gmail.com>
2022-05-01 14:30:14 +0700

    simpan perubahan hapus file gak di butuhin di branch ini

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'done_module_provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DoneModuleProvider(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: ModulePage(),
-      ),
+    return MaterialApp(
+      home: MyHome(),
     );
   }
 }
 
-class ModulePage extends StatefulWidget {
-  @override
-  State<ModulePage> createState() => _ModulePageState();
-}
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
 
-class _ModulePageState extends State<ModulePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Memulai Pemrograman Dengan Dart'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.done),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DoneModuleList(),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      body: ModuleList(),
-    );
-  }
-}
-
-class ModuleList extends StatelessWidget {
-  final List<String> _moduleList = [
-    'Modul 1 - Pengenalan Dart',
-    'Modul 2 - Memulai Pemrograman dengan Dart',
-    'Modul 3 - Dart Fundamental',
-    'Modul 4 - Control Flow',
-    'Modul 5 - Collections',
-    'Modul 6 - Object Oriented Programming',
-    'Modul 7 - Functional Styles',
-    'Modul 8 - Dart Type System',
-    'Modul 9 - Dart Futures',
-    'Modul 10 - Effective Dart',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _moduleList.length,
-      itemBuilder: (context, index) {
-        return Consumer<DoneModuleProvider>(
-          builder: (context, DoneModuleProvider data, widget) {
-            return ModuleTile(
-              moduleName: _moduleList[index],
-              isDone: data.doneModuleList.contains(_moduleList[index]),
-              onClick: () {
-                data.complete(_moduleList[index]);
-              },
-            );
-          },
-        );
-      },
-    );
-  }
-}
-
-class ModuleTile extends StatelessWidget {
-  final String moduleName;
-  final bool isDone;
-  final Function() onClick;
-
-  ModuleTile({
-    required this.moduleName,
-    required this.isDone,
-    required this.onClick,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final doneModuleList =
-        Provider.of<DoneModuleProvider>(context, listen: false).doneModuleList;
-    return ListTile(
-      title: Text("$moduleName ${doneModuleList.length}"),
-      trailing: isDone
-          ? Icon(Icons.done)
-          : ElevatedButton(
-              child: Text('Done'),
-              onPressed: onClick,
-            ),
-    );
-  }
-}
-
-class DoneModuleList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final doneModuleList =
-        Provider.of<DoneModuleProvider>(context, listen: false).doneModuleList;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Done Module List'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(doneModuleList[index]),
-          );
-        },
-        itemCount: doneModuleList.length,
-      ),
+      body: Center(child: Text("Hello World")),
     );
   }
 }
