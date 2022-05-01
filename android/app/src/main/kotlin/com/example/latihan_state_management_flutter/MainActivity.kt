3e2e8ab2e63d4e62a148e1d07c6103aedcc1b31c
fharhanamrin028@gmail.com <fharhanamrin028@gmail.com>
2022-05-01 14:26:51 +0700

    berhasil menambahkan state management provider

---
+++ android/app/src/main/kotlin/com/example/latihan_state_management_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_state_management_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/done_module_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class DoneModuleProvider extends ChangeNotifier {
+  final List<String> _doneModuleList = [];
+
+  List<String> get doneModuleList => _doneModuleList;
+
+  void complete(String moduleName) {
+    _doneModuleList.add(moduleName);
+    notifyListeners();
+  }
+  void count(String moduleName) {
+    // _doneModuleList.add(moduleName);
+    // return doneModuleList.length;
+    notifyListeners();
+  }
+}
